54f92d72b076e524e3fc2e9dc7eed7ee6ca1aff5
CST\YUY <yuy@canic.com>
2018-11-15 18:08:22 +0800

    1

--- src/applications/run.sh
+++ src/applications/run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 echo "...removing project..."
-rm -rf go-shell
+rm -rf goshell
+
+echo "...show delete result..."
+ls
 
 echo "...cloning project..."
 git clone https://github.com/leighyu1983/go-shell.git
+
+echo "...mv go-shell goshell..."
+mv go-shell goshell
+
+echo "...show mv result..."
+ls
+
+echo "...change dir..."
+cd /leigh/code/goshell/src/applications
 
 echo "...set linux temp env..."
 CGO_ENABLED=0 GOOS=linux
